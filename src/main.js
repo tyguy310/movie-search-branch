ef9355b1629641717bbbae6c79cd716233098592
Heathcliff <dennis.nathan@gmail.com>
2016-07-26 10:12:00 -0600

    Added split and join methods to movieTitle so that when added to the url, there are '+' symbols instead of spaces.

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
 $('document').ready(function() {
   $('form').on('submit', function(event) {
     event.preventDefault();
-    var movieTitle = $('#movieSearch').val();
+    var movieTitle = $('#movieSearch').val().split(' ').join('+');
     findMovieWithTitle(movieTitle);
-  })
+  });
 
   function findMovieWithTitle(movie) {
-    $.ajax {
-      url: 'http://www.omdbapi.com/?t=' + movie;
-    }
-    .done(function(movie) {
+    $.ajax ({
+      url: 'http://www.omdbapi.com/?t=' + movie
+    }).done(function(movie) {
       console.log(movie);
-    })
+    }).catch(function(error) {
+      console.log(error);
+    });
   }
-})
+});
